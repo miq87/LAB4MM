97819cc1931783bef7c245b2f3df86b7cab0e50f
miq87 <miq3l@wp.pl>
2020-06-10 11:50:11 +0200

    add comments object json

--- src/app/services/json.service.ts
+++ src/app/services/json.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 import { PostClass } from '../post-class';
+import { Comments } from '../interfaces/comments';
+
 
 @Injectable({
 providedIn: 'root'
@@ -16,15 +18,25 @@ export class JsonService {
     const url = `https://jsonplaceholder.typicode.com/${name}`;
     return this.http.get<PostClass[]>(url);
   }
-
   getObject(name: string, id: number): Observable<PostClass> {
     const url = `https://jsonplaceholder.typicode.com/${name}/${id}`;
     return this.http.get<PostClass>(url);
   }
-
   getByUserId(name: string, id: number): Observable<PostClass[]> {
     const param = new HttpParams().set('userId', id.toString())
     const url = `https://jsonplaceholder.typicode.com/${name}`;
     return this.http.get<PostClass[]>(url, { params: param });
   }
+  // ZADANIE 2 - start
+  getByPostId(name: string, id: number): Observable<Comments[]> {
+    const param = new HttpParams().set('postId', id.toString())
+    const url = `https://jsonplaceholder.typicode.com/${name}`;
+    return this.http.get<Comments[]>(url, { params: param });
+  }
+  getObject2(name: string, id: number): Observable<Comments[]> {
+    const url = `https://jsonplaceholder.typicode.com/${name}/${id}`;
+    return this.http.get<Comments[]>(url);
+  }
+  // ZADANIE 2 - end
+
 }
